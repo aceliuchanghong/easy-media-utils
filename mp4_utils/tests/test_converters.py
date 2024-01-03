4eae621e42010e0d70c2dcd6357db0a6d83b2112
Lawrence-AIP <liuch@allinfinance.com>
2024-01-03 15:27:59 +0800

    Converter-规范测试代码

--- mp4_utils/tests/test_converters.py
+++ mp4_utils/tests/test_converters.py
@@ -2,15 +2,34 @@
 from mp4_utils.converters.mp4_to_gif import MP4ToGIFConverter
 from mp4_utils.converters.mp4_to_mp3 import MP4ToMP3Converter
 from mp4_utils.converters.mp4_to_pngs import MP4ToPNGsConverter
+from mp4_utils.core.mp4_handler import MP4Handler
+
+
+def main(converter_type, file_path='../../testfiles/onboard_cover.mp4') -> MP4Handler:
+    """
+    Convert MP4 each frame.
+    :param converter_type:
+    :param ans_path:
+    :param file_path:
+    """
+    Converter = {
+        "gif": MP4ToGIFConverter,
+        "mp3": MP4ToMP3Converter,
+        "img": MP4ToPNGsConverter,
+    }
+    # converter = Converter[converter_type](file_path)
+    converter = Converter.get(converter_type)(file_path)
+
+    return converter
+
 
 if __name__ == "__main__":
     file_path = '../../testfiles/onboard_cover.mp4'
     gif_out_path = '../../testfiles/out/output.gif'
     mp3_out_path = '../../testfiles/out/output.mp3'
     pngs_out_path = '../../testfiles/out/pngs_output'
-    converter = MP4ToGIFConverter(file_path)
-    converter.process(gif_out_path)
-    converter2 = MP4ToMP3Converter(file_path)
-    converter2.process(mp3_out_path)
-    converter3 = MP4ToPNGsConverter(file_path)
-    converter3.process(pngs_out_path)
+
+    converter_type = "gif"
+    ans_path = gif_out_path
+    converter = main(converter_type, file_path)
+    converter.process(ans_path)
