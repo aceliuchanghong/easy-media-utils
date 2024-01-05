2db42db232d5b733e47c05243cf5804d246e61c6
Lawrence-AIP <liuch@allinfinance.com>
2024-01-05 16:20:39 +0800

    封版-改一下项目名称

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='media-utils',
+    name='easy-media-utils',
     version='0.1.0',  # Update the version number for new releases
     author='Sisconsavior',
     author_email='aceliuchanghong@gmail.com',
